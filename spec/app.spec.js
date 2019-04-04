bbe314f56b43a9d7a4a96f99fecaa03eb3f57d36
CJ Joulain <joulain@gmail.com>
2019-04-03 18:37:38 -0700

    Update Jasmine test to add a second example.

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,5 +1,11 @@
 describe("/", function() {
-  it("contains spec with an expectation", function() {
+  it("always passes because true is true", function() {
     expect(true).toBe(true);
+  });
+});
+
+describe("/", function() {
+  it("this test should fail", function() {
+    expect(false).toBe(true);
   });
 });
